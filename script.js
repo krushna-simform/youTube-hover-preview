36e650fe96e3e611ca8ddd81ba6d619dcd5fb8e5
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-13 09:58:39 +0530

    solve mute unmute issue

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const videoContainer = document.querySelector(".video-container");
-    const video = document.querySelector(".preview-video");
-    const thumbnail = document.querySelector(".thumbnail");
-    const playPauseBtn = document.querySelector("#playPauseBtn");
-    const muteBtn = document.querySelector("#muteBtn");
+const videoContainer = document.querySelector(".video-container");
+const video = document.querySelector(".preview-video");
+const thumbnail = document.querySelector(".thumbnail");
+const playPauseBtn = document.querySelector("#playPauseBtn");
+const muteBtn = document.querySelector("#muteBtn");
 
-    const updatePlauPauseBtn = () => {
-        playPauseBtn.textContent = video.paused ? "play" : "pause";
-    }
+const updatePlauPauseBtn = () => {
+    playPauseBtn.textContent = video.paused ? "play" : "pause";
+}
 
-    const updateMuteIcon = () => {
-        muteBtn.textContent = video.muted ? "mute" : "unmute"
-    }
+const updateMuteBtn = () => {
+    muteBtn.textContent = video.muted ? "unmute" : "mute"
+}
 
-    videoContainer.addEventListener("mouseenter", () => {
-        video.play();
-        video.style.opacity = "1";
-        thumbnail.style.opacity = "0"
-        updatePlauPauseBtn();
-    })
-
-    videoContainer.addEventListener("mouseleave", () => {
-        video.pause();
-        video.style.opacity = "0";
-        thumbnail.style.opacity = "1"
-        updatePlauPauseBtn();
-    })
-
-
-    playPauseBtn.addEventListener("click", () => {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
-        updatePlauPauseBtn();
-    })
-
-    muteBtn.addEventListener("click", () => {
-        video.muted = !video.muted;
-        updateMuteIcon();
-    })
+videoContainer.addEventListener("mouseenter", () => {
+    video.play();
+    video.style.opacity = "1";
+    thumbnail.style.opacity = "0"
+    updatePlauPauseBtn();
 })
+
+videoContainer.addEventListener("mouseleave", () => {
+    video.pause();
+    video.style.opacity = "0";
+    thumbnail.style.opacity = "1"
+    updatePlauPauseBtn();
+})
+
+
+playPauseBtn.addEventListener("click", () => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    updatePlauPauseBtn();
+})
+
+muteBtn.addEventListener("click", () => {
+    video.muted = !video.muted;
+    updateMuteBtn();
+})  
